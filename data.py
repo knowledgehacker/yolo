Fix class_proids is feed wrong value

data.py
@@ -129,7 +129,7 @@ def batch(image_dir, chunks, test=False):
     inp_feed_val = np.array(image_batch)
     loss_feed_val = {
         'class_probs': np.array(probs_batch),
-        'class_proids': np.array(probs_batch),
+        'class_proids': np.array(proids_batch),
         'object_proids': np.array(confs_batch),
         'coords': np.array(coord_batch)
     }