940f45e69532f38e8bd26f4a04f203662768a426
ming lin <linming84@126.com>
2023-01-31 20:38:29 +0800

    Fix compose, conv layer not followed by batch normalization, use bias

--- utils/compose.py
+++ utils/compose.py
@@ -11,7 +11,7 @@ def ConvBatchLReLu(x, filter, size, stride, padding_mode, data_format, index, tr
     if data_format == "channels_first":
         bn_axis = 1
     x = BatchNormalization(axis=bn_axis, name='norm_{}'.format(index), trainable=trainable)(x)
-    x = LeakyReLU(alpha=0.1)(x)
+    x = LeakyReLU(alpha=0.1, name="relu_{}".format(index))(x)
 
     return x
 
@@ -22,6 +22,25 @@ def ConvBatchLReLu_loop(x, convs, padding_mode, data_format, index, trainable):
         index += 1
 
     return x
+
+
+def ConvLReLu(x, filter, size, stride, padding_mode, data_format, index, trainable):
+    x = Conv2D(filter, kernel_size=(size, size), strides=(stride, stride),
+               padding=padding_mode, data_format=data_format, name='conv_{}'.format(index),
+               use_bias=True, trainable=trainable)(x)
+    x = LeakyReLU(alpha=0.1, name="relu_{}".format(index))(x)
+
+    return x
+
+
+def Conv(x, filter, size, stride, padding_mode, data_format, index, trainable):
+    return Conv2D(filter, kernel_size=(size, size), strides=(stride, stride),
+                  padding=padding_mode, data_format=data_format, name='conv_{}'.format(index),
+                  use_bias=True, trainable=trainable)(x)
+
+
+def MaxPool(x, size, data_format, name):
+    return MaxPool2D(pool_size=(size, size), data_format=data_format, name=name)(x)
 
 
 def FullyConnRelu(x, out_size):
