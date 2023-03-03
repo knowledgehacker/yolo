Fix loss calculation logic: 1) Use BCE instead of square errors for objectness(confidence), class loss. 2) Adjust b_x/y with c_x/y to calculate intersect area, so use [?, H, W, …] instead of [?, H*W, …] for ground truth input and prediction output. 3) For ground truth boxes and cell offsets, the coordinates are arranged in (H, W) instead of (W, H) cell.

data.py
@@ -60,10 +60,10 @@ C, labels = config.C, config.CLASSES
 def batch(image_dir, chunks, test=False):
     image_batch = []
 
-    class_probs_batch = []
-    #class_mask_batch = []
+    cls_batch = []
+    #cls_mask_batch = []
     conf_batch = []
-    coords_batch = []
+    coord_batch = []
     box_mask_batch = []
 
     for chunk in chunks:
@@ -82,11 +82,11 @@ def batch(image_dir, chunks, test=False):
         img = resize_input(img)
 
         # Calculate placeholders' values
-        class_probs = np.zeros([H*W, B, C])
-        #class_mask = np.zeros([H*W, B, C])
-        conf = np.zeros([H*W, B])
-        coords = np.zeros([H*W, B, 4])
-        box_mask = np.zeros([H*W, B])
+        cls = np.zeros([H, W, B, C])
+        #cls_mask = np.zeros([H, W, B, C])
+        conf = np.zeros([H, W, B, 1])
+        coord = np.zeros([H, W, B, 4])
+        box_mask = np.zeros([H, W, B])
 
         # Calculate regression target, normalize the items in the loss formula
         grid_w = 1. * w / W
@@ -103,7 +103,7 @@ def batch(image_dir, chunks, test=False):
                 return None, None
             grid_cx = int(cx)
             grid_cy = int(cy)
-            grid_cell = grid_cy * W + grid_cx
+            #grid_cell = grid_cy * W + grid_cx
 
             # you should handle obj[3]/[4] first, then obj[1]/[2], since obj[1]/[2] is used in obj[3]/[4]
             obj[3] = float(obj[3] - obj[1]) / grid_w
@@ -112,36 +112,40 @@ def batch(image_dir, chunks, test=False):
             obj[2] = cy - grid_cy
 
             # Calculate placeholders' values
-            class_probs[grid_cell, :, :] = [[0.] * C] * B
-            class_probs[grid_cell, :, labels.index(obj[0])] = 1.
-            #class_mask[grid_cell, :, :] = [[1.] * C] * B
-            conf[grid_cell, :] = [1.] * B
+            cls[grid_cy, grid_cx, :, :] = [[0.] * C] * B
+            cls[grid_cy, grid_cx, :, labels.index(obj[0])] = 1.
+            #cls_mask[grid_cy, grid_cx, :, :] = [[1.] * C] * B
+            conf[grid_cy, grid_cx, :] = [[1.]] * B
             anchors = np.reshape(config.anchors, [B, 2])
-            coords[grid_cell, :, 0:2] = [obj[1:3]] * B
-            coords[grid_cell, :, 2:4] = np.log([obj[3:5]] * B / anchors + 1e-8)
+            coord[grid_cy, grid_cx, :, 0:2] = [obj[1:3]] * B
+            coord[grid_cy, grid_cx, :, 2:4] = np.log(clip_by_value([obj[3:5]] * B / anchors, 1e-9, 1e9))
 
             best_iou, best_anchor = find_best_anchor(obj, anchors)
             if best_iou > 0:
-                box_mask[grid_cell, best_anchor] = 1.
+                box_mask[grid_cy, grid_cx, best_anchor] = 1.
 
         image_batch.append(img)
 
-        class_probs_batch.append(class_probs)
-        #class_mask_batch.append(class_mask)
+        cls_batch.append(cls)
+        #cls_mask_batch.append(cls_mask)
         conf_batch.append(conf)
-        coords_batch.append(coords)
+        coord_batch.append(coord)
         box_mask_batch.append(box_mask)
 
     inp_feed_val = np.array(image_batch)
     loss_feed_val = {
-        'class_probs': np.array(class_probs_batch),
-        #'class_mask': np.array(class_mask_batch),
+        'cls': np.array(cls_batch),
+        #'cls_mask': np.array(cls_mask_batch),
         'conf': np.array(conf_batch),
-        'coords': np.array(coords_batch),
+        'coord': np.array(coord_batch),
         'box_mask': np.array(box_mask_batch)
     }
 
     return inp_feed_val, loss_feed_val
+
+
+def clip_by_value(value, lb, ub):
+    return np.minimum(np.maximum(value, lb), ub)
 
 
 def find_best_anchor(obj, anchors):
@@ -166,6 +170,7 @@ def find_best_anchor(obj, anchors):
             best_anchor = k
 
     return best_iou, best_anchor
+
 
 def get_batch_num(data, batch_size):
     size = len(data)

net/fast_yolo.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import config
 from net.small import Small
-from utils.iou import cal_iou
+from utils.iou import cal_iou, create_cell_xy
 
 import tensorflow._api.v2.compat.v1 as tf
 tf.disable_v2_behavior()
@@ -34,11 +34,11 @@ class FastYolo(object):
         return net_out, pretrained_model
 
     """
-    reference to yolo_loss in yad2k/models/keras_yolo.py
-    TODO: get the detector mask, that is, find the best anchor for each ground truth box.
-    https://github.com/allanzelener/YAD2K/blob/a42c760ef868bc115e596b56863dc25624d2e756/yad2k/models/keras_yolo.py#L66
+    reference to loss_layer in wizyoung/YOLOv3_TensorFlow/model.py
+    https://github.com/wizyoung/YOLOv3_TensorFlow/blob/8776cf7b2531cae83f5fc730f3c70ae97919bfd6/model.py#L192
     """
-    def opt(self, net_out, nd_class_probs, nd_conf, nd_coords, box_mask):
+
+    def opt(self, net_out, nd_cls, nd_conf, nd_coord, box_mask):
         # parameters
         class_scale = config.class_scale
         obj_scale = config.object_scale
@@ -46,66 +46,65 @@ class FastYolo(object):
         coord_scale = config.coord_scale
         print('scales  = {}'.format([class_scale, obj_scale, noobj_scale, coord_scale]))
 
+        batch_num = tf.cast(tf.shape(net_out)[0], tf.float32)
+
+        anchors = np.reshape(config.anchors, [1, 1, 1, B, 2])
+        # shape of [H, W, B, 2] instead of [?, H, W, B, 2]
+        cell_xy = tf.concat(B * [create_cell_xy()], 2)
+
+        # handle net output feature map
         nd_net_out = tf.reshape(net_out, [-1, H, W, B, (C + 1 + 4)])
 
-        # adjust class probability, objectness(confidence), coordinate parts of net_out for loss calculation
-        adjusted_class_prob = tf.nn.softmax(nd_net_out[:, :, :, :, :C])
-        adjusted_class_prob = tf.reshape(adjusted_class_prob, [-1, H*W, B, C])
+        adjusted_cls, adjusted_conf, adjusted_coord = tf.split(nd_net_out, [C, 1, 4], axis=-1)
 
-        adjusted_conf = tf.sigmoid(nd_net_out[:, :, :, :, C])
-        adjusted_conf = tf.reshape(adjusted_conf, [-1, H*W, B, 1])
-
-        nd_coords_predict = nd_net_out[:, :, :, :, C+1:]
-        nd_coords_predict = tf.reshape(nd_coords_predict, [-1, H*W, B, 4])
-        # sigmoid to make sure nd_coords_predict[:, :, :, 0:2] is positive
-        adjusted_coords_xy = tf.sigmoid(nd_coords_predict[:, :, :, 0:2])
-        adjusted_coords_wh = nd_coords_predict[:, :, :, 2:4]
-        adjusted_coords_predict = tf.concat([adjusted_coords_xy, adjusted_coords_wh], 3)
-
-        adjusted_net_out = tf.concat([adjusted_class_prob, adjusted_conf, adjusted_coords_predict], 3)
+        # adjusted_coord[:, :, :, :, 0:2] is (t_x, t_y)
+        adjusted_coord_xy = tf.sigmoid(adjusted_coord[:, :, :, :, 0:2])
+        # adjusted_coord[:, :, :, :, 2:4] is (t_w, t_h)
+        adjusted_coord_wh = tf.clip_by_value(adjusted_coord[:, :, :, :, 2:4], -9, 9)
+        adjusted_coord = tf.concat([adjusted_coord_xy, adjusted_coord_wh], -1)
 
         """
-        confidence weight, positive and negative samples overlap, and some samples are neither positive nor negative ones
-        (the bounding boxes with highest iou >= threshold, but not responsible for any ground truth object detection).
+        confidence part, some samples are neither positive nor negative ones
+        (the boxes with highest iou >= threshold, but not responsible for detection on any ground truth object).
         """
         # positive samples(bounding boxes with the highest iou)' ground truth confidence is iou
-        positive = box_mask
+        positive = tf.expand_dims(box_mask, -1)
 
         # negative samples(bounding boxes with highest iou < threshold)' ground truth confidence is 0
-        iou = cal_iou(adjusted_coords_predict, nd_coords)
-        best_box = tf.equal(iou, tf.reduce_max(iou, axis=2, keepdims=True))
+        iou = cal_iou(adjusted_coord, nd_coord, cell_xy, anchors)
+        best_box = tf.equal(iou, tf.reduce_max(iou, axis=-1, keepdims=True))
         best_box_ge_thres = tf.equal(best_box, tf.greater_equal(iou, config.IOU_THRESHOLD))
-        negative = (1.0 - tf.to_float(best_box_ge_thres)) * (1.0 - box_mask)
+        negative = (1.0 - tf.expand_dims(tf.to_float(best_box_ge_thres), -1)) * (1.0 - positive)
 
-        conf_weight = obj_scale * positive + noobj_scale * negative
-
-        """
-        class weight, multiply positive to get the positive samples
-        """
-        box_class = tf.concat(C * [tf.expand_dims(positive, -1)], 3)
-        #class_weight = class_scale * class_mask * box_class
-        class_weight = class_scale * box_class
+        bce_conf = tf.nn.sigmoid_cross_entropy_with_logits(logits=adjusted_conf, labels=nd_conf)
+        conf_obj_loss = obj_scale * tf.reduce_sum(positive * bce_conf) / batch_num
+        conf_noobj_loss = noobj_scale * tf.reduce_sum(negative * bce_conf) / batch_num
+        conf_loss = conf_obj_loss + conf_noobj_loss
 
         """
-        coordinate weight, multiply positive to get the positive samples.
+        class part, multiply positive to get the positive samples.
+        """
+        cls_mask = tf.concat(C * [positive], -1)
+
+        bce_class = tf.nn.sigmoid_cross_entropy_with_logits(logits=adjusted_cls, labels=nd_cls)
+        class_loss = class_scale * tf.reduce_sum(cls_mask * bce_class) / batch_num
+
+        """
+        coordinate part, multiply positive to get the positive samples.
         use (b_x - c_x, b_y - c_y), (b_w / p_w, b_h / p_h) in coordinate loss, is it correct???
         I think so, (width, height) loss should be irrelevant to anchor size (width, height).
         """
-        box_coord = tf.concat(4 * [tf.expand_dims(positive, -1)], 3)
-
+        coord_mask = tf.concat(4 * [positive], -1)
         # the bigger the box is, the smaller its weight is. the trick to improve detection on small objects???
-        true_wh = tf.exp(nd_coords[:, :, :, 2:4]) * np.reshape(config.anchors, [1, 1, config.B, 2])
-        coord_ratio = 2 - (true_wh[:, :, :, 0] / config.W) * (true_wh[:, :, :, 1] / config.H)
-        coord_ratio = tf.concat(4 * [tf.expand_dims(coord_ratio, -1)], 3)
+        true_wh = tf.exp(nd_coord[:, :, :, :, 2:4]) * anchors
+        coord_ratio = 2 - (true_wh[:, :, :, :, 0] / W) * (true_wh[:, :, :, :, 1] / H)
+        coord_ratio = tf.concat(4 * [tf.expand_dims(coord_ratio, -1)], -1)
 
-        coord_weight = coord_scale * box_coord * coord_ratio
+        se_coord = (adjusted_coord - nd_coord) ** 2
+        coord_loss = coord_scale * tf.reduce_sum(coord_ratio * coord_mask * se_coord) / batch_num
 
-        # do not normalize with anchors in data.py, so do not adjust coordinate width and height here
-        true = tf.concat([nd_class_probs, tf.expand_dims(nd_conf, 3), nd_coords], 3)
-        weights = tf.concat([class_weight, tf.expand_dims(conf_weight, 3), coord_weight], 3)
-        weighted_square_error = weights * ((adjusted_net_out - true) ** 2)
-        weighted_square_error = tf.reshape(weighted_square_error, [-1, H*W * B * (C + 1 + 4)])
-        loss_op = 0.5 * tf.reduce_mean(tf.reduce_sum(weighted_square_error, 1), name="loss")
+        # total loss
+        loss_op = tf.add_n([conf_loss, class_loss, coord_loss], name="loss")
 
         return loss_op
 

train.py
@@ -54,19 +54,19 @@ def train():
         #To be able to feed with batches of different size, the first dimension should be None
         image_ph = tf.placeholder(dtype=tf.float32, shape=config.placeholder_image_shape, name="image_ph")
         bounding_box_ph_dict = {
-            "class_probs": tf.placeholder(dtype=tf.float32, shape=(None, H*W, B, C), name="class_probs_ph"),
-            #"class_mask": tf.placeholder(dtype=tf.float32, shape=(None, H*W, B, C), name="class_mask_ph"),
-            "conf": tf.placeholder(dtype=tf.float32, shape=(None, H*W, B), name="conf_ph"),
-            "coords": tf.placeholder(dtype=tf.float32, shape=(None, H*W, B, 4), name="coords_ph"),
-            "box_mask": tf.placeholder(dtype=tf.float32, shape=(None, H * W, B), name="box_mask_ph")
+            "cls": tf.placeholder(dtype=tf.float32, shape=(None, H, W, B, C), name="cls_ph"),
+            #"cls_mask": tf.placeholder(dtype=tf.float32, shape=(None, H, W, B, C), name="cls_mask_ph"),
+            "conf": tf.placeholder(dtype=tf.float32, shape=(None, H, W, B, 1), name="conf_ph"),
+            "coord": tf.placeholder(dtype=tf.float32, shape=(None, H, W, B, 4), name="coord_ph"),
+            "box_mask": tf.placeholder(dtype=tf.float32, shape=(None, H, W, B), name="box_mask_ph")
         }
         dropout_keep_prob_ph = tf.placeholder(tf.float32, name="dropout_keep_prob_ph")
 
         net_out_op, pretrained_model = model.forward(image_ph, config.input_shape, config.data_format,
                                                      dropout_keep_prob_ph, True)
-        loss_op = model.opt(net_out_op, bounding_box_ph_dict["class_probs"], #bounding_box_ph_dict["class_mask"],
+        loss_op = model.opt(net_out_op, bounding_box_ph_dict["cls"], #bounding_box_ph_dict["cls_mask"],
                             bounding_box_ph_dict["conf"],
-                            bounding_box_ph_dict["coords"], bounding_box_ph_dict["box_mask"])
+                            bounding_box_ph_dict["coord"], bounding_box_ph_dict["box_mask"])
 
     with tf.Session(graph=g, config=cfg) as sess:
         """

utils/iou.py
@@ -7,14 +7,13 @@ import tensorflow._api.v2.compat.v1 as tf
 tf.disable_v2_behavior()
 
 
-def _extract_coords(coords):
+def _extract_coords(coords, cell_coords, anchors):
     # unit: grid cell, w = orig_w / grid_w, h = orig_h / grid_h.
-    #wh = (tf.exp(coords[:, :, :, 2:4]) - 1e-8) * np.reshape(config.anchors, [1, 1, config.B, 2])
-    wh = tf.exp(coords[:, :, :, 2:4]) * np.reshape(config.anchors, [1, 1, config.B, 2])
-    area = wh[:, :, :, 0] * wh[:, :, :, 1]  # unit: grid cell^2
-    xy_centre = coords[:, :, :, 0:2]  # [batch, SS, B, 2]
-    left_top = xy_centre - (wh * 0.5)  # [batch, SS, B, 2]
-    right_bottom = xy_centre + (wh * 0.5)  # [batch, SS, B, 2]
+    wh = tf.exp(coords[:, :, :, :, 2:4]) * anchors
+    area = wh[:, :, :, :, 0] * wh[:, :, :, :, 1]  # unit: grid cell^2
+    xy_centre = coords[:, :, :, :, 0:2] + cell_coords  # [batch, W, H, B, 2]
+    left_top = xy_centre - (wh * 0.5)  # [batch, W, H, B, 2]
+    right_bottom = xy_centre + (wh * 0.5)  # [batch, W, H, B, 2]
 
     return left_top, right_bottom, area
 
@@ -22,16 +21,15 @@ def _extract_coords(coords):
 def _calc_intersects(left_top_1, right_bottom_1, left_top_2, right_bottom_2):
     left_top_intersect = tf.maximum(left_top_1, left_top_2)
     right_bottom_intersect = tf.minimum(right_bottom_1, right_bottom_2)
-    wh_intersect = right_bottom_intersect - left_top_intersect
-    wh_intersect = tf.maximum(wh_intersect, 0.0)
-    area_intersect = wh_intersect[:, :, :, 0] * wh_intersect[:, :, :, 1]
+    wh_intersect = tf.maximum(right_bottom_intersect - left_top_intersect, 0.)
+    area_intersect = wh_intersect[:, :, :, :, 0] * wh_intersect[:, :, :, :, 1]
 
     return left_top_intersect, right_bottom_intersect, area_intersect
 
 
-def cal_iou(coords_predict, coords_true):
-    left_top_predict, right_bottom_predict, area_predict = _extract_coords(coords_predict)
-    left_top_true, right_bottom_true, area_true = _extract_coords(coords_true)
+def cal_iou(coords_predict, coords_true, cell_coords, anchors):
+    left_top_predict, right_bottom_predict, area_predict = _extract_coords(coords_predict, cell_coords, anchors)
+    left_top_true, right_bottom_true, area_true = _extract_coords(coords_true, cell_coords, anchors)
     _, _, area_intersect = _calc_intersects(left_top_predict, right_bottom_predict,
                                             left_top_true, right_bottom_true)
 
@@ -39,3 +37,18 @@ def cal_iou(coords_predict, coords_true):
     iou = area_intersect / (area_predict + area_true - area_intersect)
 
     return iou
+
+
+def create_cell_xy():
+    # use some broadcast tricks to get the mesh coordinates
+    w, h = config.W, config.H
+
+    grid_x = tf.range(w, dtype=tf.int32)
+    grid_y = tf.range(h, dtype=tf.int32)
+    grid_x, grid_y = tf.meshgrid(grid_x, grid_y)
+    x_offset = tf.reshape(grid_x, (-1, 1))
+    y_offset = tf.reshape(grid_y, (-1, 1))
+    x_y_offset = tf.concat([x_offset, y_offset], axis=-1)
+    x_y_offset = tf.cast(tf.reshape(x_y_offset, [h, w, 1, 2]), tf.float32)
+
+    return x_y_offset