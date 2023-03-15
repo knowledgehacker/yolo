647980de17e5892f37e9d659d788d2026c2af74d
ming lin <linming84@126.com>
2023-03-15 14:10:31 +0800

    Change darknet config file name

--- convert.sh
+++ convert.sh
@@ -8,9 +8,9 @@ DIR=data/weights
 #NET=darknet19
 NET=darknet19_448
 # by default the model instead of weights are saved, to enbale load_weights, pass argument "--weights_only"
-python3 convert.py --weights_only --plot_model cfg/${NET}_-2.cfg ${DIR}/${NET}.weights ${DIR}/${NET}.h5
+python3 convert.py --weights_only --plot_model cfg/${NET}_-.cfg ${DIR}/${NET}.weights ${DIR}/${NET}.h5
 
 #NET=extraction
 # by default the model instead of weights are saved, to enbale load_weights, pass argument "--weights_only"
-#python3 convert.py --weights_only --plot_model cfg/${NET}_-2.cfg ${DIR}/${NET}.weights ${DIR}/${NET}.h5
+#python3 convert.py --weights_only --plot_model cfg/${NET}_-.cfg ${DIR}/${NET}.weights ${DIR}/${NET}.h5
 
